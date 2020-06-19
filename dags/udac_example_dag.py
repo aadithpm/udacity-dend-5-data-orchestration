811a26e5d1b667d7a7434075502f4ae1979f8f3e
Aadith PM <aadithpm@gmail.com>
2020-06-19 20:57:58 +0000

    Fixing some errors in the DAG

--- dags/udac_example_dag.py
+++ dags/udac_example_dag.py
@@ -1,20 +1,20 @@
 from datetime import datetime, timedelta
 import os
 from airflow import DAG
+from helpers import SqlQueries
 from airflow.operators.dummy_operator import DummyOperator
 from airflow.operators import (StageToRedshiftOperator, LoadFactOperator,
                                 LoadDimensionOperator, DataQualityOperator)
-from helpers import SqlQueries
 
 # AWS_KEY = os.environ.get('AWS_KEY')
 # AWS_SECRET = os.environ.get('AWS_SECRET')
 
 default_args = {
     'owner': 'udacity',
-    'start_date': datetime(2019, 1, 12),
+    'start_date': datetime(2020, 6, 18),
     'depends_on_past': False,
-    'retries': 2,
-    'retry_delay': timedelta(seconds=600),
+    'retries': 3,
+    'retry_delay': timedelta(seconds=300),
     'catchup': False
 }
 
@@ -26,10 +26,17 @@ dag = DAG('udac_example_dag',
 
 start_operator = DummyOperator(task_id='Begin_execution',  dag=dag)
 
+run_quality_checks = DataQualityOperator(
+    task_id='Run_data_quality_checks',
+    dag=dag,
+    redshift_conn='redshift',
+    tables=['songplays', 'users', 'songs', 'artists', 'time']
+)
+
 stage_events_to_redshift = StageToRedshiftOperator(
     task_id='Stage_events',
     dag=dag,
-    table='staging_events',
+    target_table='staging_events',
     redshift_conn='redshift',
     aws_credentials='aws_credentials',
     s3_bucket='udacity-dend',
@@ -40,7 +47,7 @@ stage_events_to_redshift = StageToRedshiftOperator(
 stage_songs_to_redshift = StageToRedshiftOperator(
     task_id='Stage_songs',
     dag=dag,
-    table='staging_songs',
+    target_table='staging_songs',
     redshift_conn='redshift',
     aws_credentials='aws_credentials',
     s3_bucket='udacity-dend',
@@ -48,11 +55,12 @@ stage_songs_to_redshift = StageToRedshiftOperator(
     json_path=''
 )
 
+
 load_songplays_table = LoadFactOperator(
     task_id='Load_songplays_fact_table',
     dag=dag,
     redshift_conn='redshift',
-    table='songplays',
+    target_table='songplays',
     select_query=SqlQueries.songplay_table_insert
 )
 
@@ -60,16 +68,16 @@ load_user_dimension_table = LoadDimensionOperator(
     task_id='Load_user_dim_table',
     dag=dag,
     redshift_conn='redshift',
-    table='users',
+    target_table='users',
     truncate_table=True,
     select_query=SqlQueries.user_table_insert
 )
 
 load_song_dimension_table = LoadDimensionOperator(
     task_id='Load_song_dim_table',
-    dag=dag.
+    dag=dag,
     redshift_conn='redshift',
-    table='songs',
+    target_table='songs',
     truncate_table=True,
     select_query=SqlQueries.song_table_insert
 )
@@ -78,7 +86,7 @@ load_artist_dimension_table = LoadDimensionOperator(
     task_id='Load_artist_dim_table',
     dag=dag,
     redshift_conn='redshift',
-    table='artists',
+    target_table='artists',
     truncate_table=True,
     select_query=SqlQueries.artist_table_insert
 )
@@ -87,19 +95,14 @@ load_time_dimension_table = LoadDimensionOperator(
     task_id='Load_time_dim_table',
     dag=dag,
     redshift_conn='redshift',
-    table='time',
+    target_table='time',
     truncate_table=True,
     select_query=SqlQueries.time_table_insert
 )
 
-run_quality_checks = DataQualityOperator(
-    task_id='Run_data_quality_checks',
-    dag=dag,
-    redshift_conn='redshift',
-    tables=['songplays', 'users', 'songs', 'artists', 'time']
-)
-
 end_operator = DummyOperator(task_id='Stop_execution',  dag=dag)
+
+
 
 start_operator >> stage_events_to_redshift
 start_operator >> stage_songs_to_redshift
